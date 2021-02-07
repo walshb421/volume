25fdececf891cd6fa176e648e114ea7213bf015f
Benjiman Walsh <walshb@oregonstate.edu>
2021-02-06 18:58:06 -0800

    Wrote program that calculates volume

---
+++ volume.py
@@ -0,0 +1,11 @@
+
+
+def calc_volume(x, y, z):
+    return x * y * z
+
+
+height = input("Height: ")
+length = input("Length: ")
+width = input("Width: ")
+
+print(calc_volume(height, length, width))
